aeb9538f0a0d38c2d8a4c7eebd64c89ce3028c56
Elizaveta <134635819+elisprog@users.noreply.github.com>
2023-12-04 00:49:26 +0300

    верстка форм и начало js для них

--- index-forms.js
+++ index-forms.js
@@ -1,3 +1,49 @@
+let name = document.querySelector('.name');
+let surname = document.querySelector('.surname');
+let email = document.querySelector('.email');
+let formName = document.querySelector('.formName');
+let formSurname = document.querySelector('.formSurname');
+let formEmail = document.querySelector('.formEmail');
+/*formName.onsubmit = function(evt) {
+    if (name.value == '') {
+      evt.preventDefault();
+    }
+  };
+  formSurname.onsubmit = function(evt) {
+    if (surname.value == '') {
+      evt.preventDefault();
+    }
+  };
+  formEmail.onsubmit = function(evt) {
+    if (email.value == '') {
+      evt.preventDefault();
+    }
+  };*/
+
+//шаблон для регистра вводимых значений
+function inputSize() {
+let checkedName = name.value;
+name.textContent = checkedName[0].toUpperCase() + checkedName.slice(1).toLowerCase();
+
+let checkedSurname = surname.value;
+surname.textContent = checkedSurname[0].toUpperCase() + checkedSurname.slice(1).toLowerCase();
+
+let checkedEmail = email.value;
+email.textContent = checkedEmail.slice(0).toLowerCase();
+};
+
+
+//появление и удаление доп инпута с улицей и домом в зависимости от выбора доставки до двери и самовывоза, соответственно
+let hide = document.querySelector('.formStreet');
+let select = document.querySelector('.deliverySelect');
+function openHide () {
+    if (select.value === 'getByMyself') {
+    hide.classList.add('hide');
+    }
+    else hide.classList.remove('hide');
+};
+
+//передача массива столиц в выпадающий список
 const citiesList = [
 'Амстердам',
 'Андорра-ла-Велья',
@@ -54,8 +100,45 @@ for (i = 0; i<citiesList.length; i++) {
     citiesParent.append(citiesChild);
 };
 
-let finalPrice = document.querySelector('#finalPrice');
-let name = document.querySelector('.name')
+/*
+//работа с модулем Nodemailer
+const nodemailer = require('nodemailer')
 
-if 
+const transporter = nodemailer.createTransport(
+    {
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, 
+        auth: {
+            user: 'bogdanovaelis@gmail.com', 
+            pass: 'elissyele11' 
+        }
+    },
+    {
+        from: 'Mailer Test <bogdanovaelis@gmail.com>' 
+    }
+)
+
+const mailer = message => {
+    transporter.sendMail(message, (err, info) => {
+        if(err) return console.log(err)
+        console.log('Email sent: ', info)
+    })
+}
+
+module.exports = mailer*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
