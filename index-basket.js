07ee735c07456cc96c79b0b8bf841d63dff994c1
[mironmari] <[mironrnko.mv@mail.ru>
2023-12-05 15:12:32 +0100

    basket problem delete

--- index-basket.js
+++ index-basket.js
@@ -1,40 +1,85 @@
-
-let basketProducts = document.querySelector('.basket_products')
+let basketProducts = document.querySelector('.basket_products');
 
 function getBasketData() {
 
-  for(let i=0; i<localStorage.length; i++) {
-    let key = localStorage.key(i);
-    let basket = JSON.parse(localStorage.getItem(key)) ;
-    createBasketProduct (basket)
-  return console.log(basket);
-}}
+  fetch('http://localhost:3001/chosenProducts')
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.length>0) {
+    data.forEach(element => {createBasketProduct (element)
+
+  });
+
+  let btnClose = document.querySelectorAll('.close')
+  console.log("btnClose", btnClose)
+  btnClose.forEach((elem) => {
+    elem.onclick = function (e) {
+        let targetdeleteProduct = e.target
+        console.log("targetdeleteProduct", targetdeleteProduct)
+        targetdeleteProduct.forEach((element) => {
+                try {
+                    fetch('http://localhost:3001/chosenProducts', {
+                        method: 'DELETE',
+                        body: JSON.stringify({
+                            id: element.id,
+                            brand: element.brand,
+                            name: element.name,
+                            size: element.size,
+                            ranking: element.ranking,
+                            price: element.price,
+                            discount: element.discount,
+                            image: element.image,
+                        }),
+                        headers: {
+                            'Content-type': 'application/json',
+                        },
+                    }).then((response) => {
+                        console.log(response)
+                    })
+                } catch (err) {
+                    console.log(err.status)
+                }
+
+        })
+    }
+  })
+}
+  else {
+    const div = document.createElement('div')
+    div.classList.add('basket_no-products')
+    const el = `<div>В корзине нет товара</div>`
+   }
+  console.log(data)
+  })
+  .catch((error) => console.error('Ошибка:', error));
+}
 
 function createBasketProduct (basket){
   const name  = basket.name
   const price  = basket.price
   const image  = basket.image
   const div = document.createElement('div')
+
   div.classList.add('basket_products')
   const el = `
-  <div class="basket_products__container">
-  <div class="basket_div__imgAndname">
-  <img src="${image}" class="basket_div__img">
-  <div class="basket_div__nameplusminus">
-  <div class="basket_products__name">${name}</div>
-    <div class="basket_quantity">
-      <button class="basket_minus-btn" type="button" name="button"></button>
-      <input class="basket_input" type="text" value="1" id="input" />
-      <button class="basket_plus-btn" type="button" name="button"></button>
-    </div>
-  </div>
-  </div>
-  <div class="basket_div__priceAndcross">
-  <div class="basket_price">${price}</div>
-  <div class="close">&times</div>
-  </div>
-  </div>
-  <div><hr class="basket_line__gray" /></div>`
+                <div class="basket_products__container">
+                <div class="basket_div__imgAndname">
+                <img src="${image}" class="basket_div__img">
+                <div class="basket_div__nameplusminus">
+                <div class="basket_products__name">${name}</div>
+                  <div class="basket_quantity">
+                    <button class="basket_minus-btn" type="button" name="button"></button>
+                    <input class="basket_input" type="text" value="1" id="input" />
+                    <button class="basket_plus-btn" type="button" name="button"></button>
+                  </div>
+                </div>
+                </div>
+                <div class="basket_div__priceAndcross">
+                <div class="basket_price">${price}</div>
+                <button class="close" id="deleteProduct">&times</button>
+                </div>
+                </div>
+                <div><hr class="basket_line__gray" /></div>`
 
   div.insertAdjacentHTML("beforeend", el)
   basketProducts.appendChild(div)
@@ -43,6 +88,11 @@ function createBasketProduct (basket){
 document.addEventListener("DOMContentLoaded", ()=>{
   getBasketData()
 })
+
+
+
+
+
 
 
 
